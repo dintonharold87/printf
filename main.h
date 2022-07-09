b1f78382e2523eb995e717158b1a3fc0d7237841
C0llin5 <ebongcollins@gmail.com>
2022-07-09 06:23:57 -0700

    updated header file

--- main.h
+++ main.h
@@ -8,6 +8,19 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags;
 
 
 
