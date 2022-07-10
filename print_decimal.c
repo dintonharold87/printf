60ca21b421ae7dbe564628d8b7116247b647c9c8
C0llin5 <ebongcollins@gmail.com>
2022-07-10 07:58:30 -0700

    added print_number to print_decimal.c

--- print_decimal.c
+++ print_decimal.c
@@ -35,4 +35,24 @@ int print_unsigned(va_list l, flags *f)
 	(void)f;
 	return (_puts(str));
 }
+/**
+ * print_number - helper function that loops through
+ * an integer and prints all its digits
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+	else
+		n1 = n;
+	if (n1 / 10)
+		print_number(n1 / 10);
+	_putchar((n1 % 10) + '0');
+}
 
