474d6db9969d422a6c426adf13bd7503d46a5784
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-07-10 18:44:03 +0300

    Added function print_binary to print numbers in binary format

--- print_bases.c
+++ print_bases.c
@@ -42,3 +42,21 @@ int print_hex_big(va_list l, flags *f)
 	count += _puts(str);
 	return (count);
 }
+
+/**
+ * print_binary - prints a number in base 2
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct that determines
+ * if a flag is passed to _printf
+ * Description: the function calls convert() which in turns converts the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
+ */
+int print_binary(va_list l, flags *f)
+{
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 2, 0);
+
+	(void)f;
+	return (_puts(str));
+}
